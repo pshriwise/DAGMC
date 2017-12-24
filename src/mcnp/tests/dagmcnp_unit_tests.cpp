afa7334e3ce7156818d64004c098cec2fa6a6c81
shriwise <pshriwise@gmail.com>
2017-12-23 19:06:57 -0600

    Revert "Calling teardown at the end of the dagmcnp tests"
    This reverts commit 6cd602be084b014988b0e1762b16e9af80e8ddb9.

--- src/mcnp/tests/dagmcnp_unit_tests.cpp
+++ src/mcnp/tests/dagmcnp_unit_tests.cpp
@@ -29,9 +29,6 @@ class DAGMCNP5Test : public ::testing::Test {
     dagmcinit_(file, &len, ftol, &ftol_len, &parallel_mode,
                &dagmc_version, &moab_version, &max_pbl);
 
-    dagmc_teardown_();
-    
-
   }
   void setup_problem_comp() {
     std::string filename = test_file_comp;
@@ -48,8 +45,7 @@ class DAGMCNP5Test : public ::testing::Test {
     // intialise dagmc
     dagmcinit_(file, &len, ftol, &ftol_len, &parallel_mode,
                &dagmc_version, &moab_version, &max_pbl);
-    
-    dagmc_teardown_();
+
   }
 
   void setup_problem_reflecting() {
@@ -67,7 +63,6 @@ class DAGMCNP5Test : public ::testing::Test {
     // intialise dagmc
     dagmcinit_(file, &len, ftol, &ftol_len, &parallel_mode,
                &dagmc_version, &moab_version, &max_pbl);
-    dagmc_teardown_();
 
   }
 
